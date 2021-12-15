6119ba523da3460693da6f137e2cf97de3232ec8
putrapuices <puices14@gmail.com>
2021-12-15 15:40:01 +0700

    Menambahkan Fungsi Decrement Pada Halaman ( 9 )

--- src/Counter.js
+++ src/Counter.js
@@ -8,6 +8,7 @@ function Counter(props){
             <h1>Saya mesin penghitung</h1>
             <p>Count:{props.count}</p>
             <button onClick = {props.onIncrementClick}>Increment</button>
+            <button onClick = {props.onDecrementClick}>Decrement</button>
         </div>
     )
 }
@@ -23,10 +24,15 @@ function mapDispatchtoProps(dispatch){
 
     return{
         onIncrementClick : () =>{
-        console.log('tombol di Cklick');
+        console.log('tombol INCREMENET di Cklick');
         const action = {type:'INCREMENT'};
         dispatch(action);
-        }
+        },
+        onDecrementClick : () =>{
+            console.log('tombol decrement di cklick');
+            const action = {type:'DECREMENT'};
+            dispatch(action);
+            }
         }
 
 }
